d632d947334d999a5bc5e94071f6344167dda308
JulianeAlbuquerque <juliane.albq@gmail.com>
2016-05-18 10:38:04 -0300

    fix script npm js and sass

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,12 @@
-(function () {
-  var doc = document.documentElement;
+(function() {
+  var ele = document.documentElement(".years-old");
+  d = new Date();
+  year = d.getFullYear();
+  month = d.getMonth();
 
-  doc.classList.remove('no-js');
-  doc.classList.add('js');
+  if (month < 6) {
+    ele.innerHTML = (year - 1) - 1991
+  } else {
+    ele.innerHTML = year - 1991
+  }
 }());
