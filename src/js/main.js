ba422d4635cee5313c8a90147fe7c84950ffc544
JulianeAlbuquerque <juliane.albq@gmail.com>
2016-05-18 11:05:23 -0300

    fix package.json and fix js and css

--- src/js/main.js
+++ src/js/main.js
@@ -1,8 +1,8 @@
 (function() {
   var ele = document.documentElement(".years-old");
-  d = new Date();
-  year = d.getFullYear();
-  month = d.getMonth();
+      d = new Date();
+      year = d.getFullYear();
+      month = d.getMonth();
 
   if (month < 6) {
     ele.innerHTML = (year - 1) - 1991
